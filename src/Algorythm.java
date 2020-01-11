f85f11754ad05a8107e2042f70e5624700b6b89c
Pavel <pavel.demin20095@gmail.com>
2020-01-11 14:23:23 +0600

    commit 1

---
+++ src/Algorythm.java
@@ -0,0 +1,12 @@
+class Algorythm {
+    private String msg;
+    public Algorythm(){
+        msg = "Good Morning!";
+    }
+	public Algorythm(String msg){
+        this.msg = msg;
+    }
+    public  void out() {
+        System.out.println(msg);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+class Main {
+    public static void main(java.lang.String[] strings) {
+		Algorythm alg = new Algorythm();
+		alg.out();
+		Algorythm alg2 = new Algorythm("Other Alg");
+		alg2.out();
+    }
+}
